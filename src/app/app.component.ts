2fe6f65f32c12689ddc70f5dbc7f6a37af562de9
Bui The Anh <theanhbui345@gmail.com>
2022-12-13 23:35:02 +0700

    222

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'project-angular';
-=======
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'vnn.project-angular-test';
->>>>>>> 9ff0ffd188c87bf336df68e1d2bfad62c143d8ce
 }
